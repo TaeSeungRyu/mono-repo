4c7bbf35a1e51bad00b2026d755956798e6db42d
ts.Ryu <lts06069@naver.com>
2025-04-17 11:51:17 +0900

    모노레포 실제 서버와 클라이언트 - 1
    1. 서버
     - nestjs
    
    2. 클라이언트
     - nextjs
    
    3. 목표
     0) DDD 형태의 서버, 클라이언트 구조
     1) 일반 로그인 기능, 소셜 로그인 기능
     2) CRUD 형태의 리스트

--- apps/app1/index.js
+++
@@ -1,9 +0,0 @@
-import { printer, parseDateSample, debounceSample } from "my-common-utils";
-import { DEBUG } from "my-common-props";
-
-console.log("Hello from App 1!");
-printer(DEBUG, "This is a debug message.");
-console.log(DEBUG, parseDateSample("2021-01-01"));
-debounceSample(() => {
-  console.log("run debounceSample");
-});
